17629750dc26c2484b6457e64bc6ad277c66abed
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-09-14 09:25:29 +0500

    Contact Cards

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,47 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
+import { Card, Button } from 'react-bootstrap';
 
-
-function Main() {
-
-    const[userData, setUserName] = useState([]);
-    // const[userAge, setUserAge] = useState("");
-    useEffect(() => {
-        fetch('https://randomuser.me/api/?results=9')
-        .then(results => results.json())
-        .then(data =>{
-            console.log(data)
-            setUserName(data.results);
-        });
-    },[])
-    function setUserAge(){
-        alert('Connecting Call..');
-    }
+const Main = (props) => {
+    const [age, setAge] = useState(false);
 
     return (
-        <>
-          {
-          userData.map((user) => (
-
-        <div class="card mb-3 mx-auto" style={{width: '600px', margin: '45px', padding:'30px'}}>
-        <div class="row g-0" >
-        <div class="col-md-4">
-        <img src={user.picture.large} class="img-fluid rounded-start" alt="user-img"/>
+        <div  >
+          
+            <Card  style={{width: '300px', margin: '40px', padding:'40px', boxShadow:'0 0 10px 0 purple'}}>
+                <Card.Img variant="top" src= {props.image} />
+                <Card.Body>
+                    <Card.Title>Contact Name</Card.Title>
+                    <Card.Text>
+                        <p>Name:  {props.name}</p>
+                        <p>Email:{props.email} </p>
+                        <p>Nationality:{props.nationality} </p>
+                        {age && <p>Age: {props.age}</p>}
+                    </Card.Text>
+                    <Button variant="primary" onClick={() => setAge(!age)}>Show Age</Button>
+                </Card.Body>
+            </Card>
         </div>
-        <div class="col-md-8">
-        <div class="card-body" style={{color: 'purple'}} >
-          <h5 className="card-name">Name: {user.name.first} {user.name.last} </h5>
-          <h5 className='card-Gender'>Gender: {user.gender} </h5>
-          <h5 className='card-email'>Email: {user.email} </h5>
-          <h5 className='card-age'>Age: {user.dob.age} </h5>
-          <p className='text-center'><button class="btn btn-primary" onClick = {() => setUserAge()}>Call</button></p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-      ))}
-        </>
-    );}
+    )
+}
   
         
 export default Main;
